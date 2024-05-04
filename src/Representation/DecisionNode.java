7fad3a7a22cc9340cfaa701cc91faf1583b501dc
saidoubarry02 <saidoubarry12@outlook.fr>
2024-05-04 16:58:03 +0200

    Ajout des premieres classes

--- src/Representation/DecisionNode.java
+++ src/Representation/DecisionNode.java
@@ -3,22 +3,29 @@ import java.util.Scanner;
 
 
 public abstract class DecisionNode extends InnerNode{
-/*
+
     @Override
     public Node choseNext(){
         Scanner scanner = new Scanner (System.in);
         int choix;
-        System.out.println("Que choisis-tu entre les 4 options ?");
 
-        try {
-             choix = scanner.nextInt();
-        } catch (Exception e) {
-            System.out.println("Erreur : Que choisis-tu entre les 4 options ");
-            choix = scanner.nextInt();
+        while (true) {
+            System.out.print("Veuillez saisir un entier entre 1 et 4 : ");
+            if (scanner.hasNextInt()) {
+                choix = scanner.nextInt();
+                if (choix >= 1 && choix <= 4) {
+                    System.out.println("Vous avez saisi : " + choix);
+                    break;
+                } else {
+                }
+            } else {
+                System.out.println("Veuillez saisir un entier valide.");
+                scanner.next();
+            }
         }
+        scanner.close();
 
-        // Utiliser la valeur entrée par l'utilisateur (si aucune exception n'a été levée)
-        System.out.println("Vous avez entré : " + choix);
+
     }
-*/
+
 }
